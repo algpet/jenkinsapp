4e3158b4aeb2292838131599dbe5025103daecc1
algpet <litovec@gmail.com>
2017-07-13 23:18:04 +0300

    42

--- src/main/java/foo/bar/baz/Main.java
+++ src/main/java/foo/bar/baz/Main.java
@@ -7,6 +7,7 @@ public class Main {
 
     public static void main(String[] args){
         System.out.println("Started ....");
+        System.out.println(42);
         System.out.println(".... and finished");
     }
 }
