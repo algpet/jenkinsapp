ef375ea1a8c50315a43d04e5b187ac7bb29770da
algpet <litovec@gmail.com>
2017-07-13 22:43:31 +0300

    Initial commit

---
+++ src/main/java/foo/bar/baz/Main.java
@@ -0,0 +1,12 @@
+package foo.bar.baz;
+
+/**
+ * Created by algis on 17.13.7.
+ */
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("Started ....");
+        System.out.println(".... and finished");
+    }
+}
